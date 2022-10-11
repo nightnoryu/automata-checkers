cefcc8265753f9bbf39152d8b688fbfc4c2c6225
m3tro1d <qazwf734@gmail.com>
2022-10-11 21:26:34 +0300

    lab1: mock Mealy

--- lab1/Infrastructure/CSV/CInputOutputAdapter.cpp
+++ lab1/Infrastructure/CSV/CInputOutputAdapter.cpp
@@ -3,7 +3,18 @@
 MealyAutomaton CInputOutputAdapter::GetMealy(const std::string& filename)
 {
 	// TODO: implement parsing from CSV
-	return {};
+	return {
+		.states = { "a1", "a2", "a3" },
+		.inputSymbols = { "z1", "z2" },
+		.moves = {
+			{ { "a1", "z1" }, { "a3", "w1" } },
+			{ { "a1", "z2" }, { "a1", "w1" } },
+			{ { "a2", "z1" }, { "a1", "w1" } },
+			{ { "a2", "z2" }, { "a3", "w2" } },
+			{ { "a3", "z1" }, { "a1", "w2" } },
+			{ { "a3", "z2" }, { "a2", "w1" } },
+		}
+	};
 }
 
 MooreAutomaton CInputOutputAdapter::GetMoore(const std::string& filename)
