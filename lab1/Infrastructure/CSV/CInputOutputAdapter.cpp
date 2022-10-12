d89780b2afc6c8c7052586711d44bed1ffa17276
kazantsev-roman <dj.ka3anova@gmail.com>
2022-10-12 03:00:50 +0300

    lab1: implement Melay parser

--- lab1/Infrastructure/CSV/CInputOutputAdapter.cpp
+++ lab1/Infrastructure/CSV/CInputOutputAdapter.cpp
@@ -1,19 +1,87 @@
 #include "CInputOutputAdapter.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+CInputOutputAdapter::Spreadsheet CInputOutputAdapter::GetDataFromFile(const std::string& filename)
+{
+	std::ifstream input(filename);
+	if (!input.is_open())
+	{
+		throw std::runtime_error("failed to open input file for reading");
+	}
+
+	Spreadsheet spreadsheet;
+	std::string row;
+	std::string cell;
+
+	while(std::getline(input, row))
+	{
+		std::istringstream iss(row);
+		std::vector<std::string> rows;
+
+		while(std::getline(iss, cell, ','))
+		{
+			rows.push_back(cell);
+		}
+
+		spreadsheet.push_back(rows);
+	}
+
+	return spreadsheet;
+}
 
 MealyAutomaton CInputOutputAdapter::GetMealy(const std::string& filename)
 {
-	// TODO: implement parsing from CSV
+	Spreadsheet spreadsheet = GetDataFromFile(filename);
+
+	std::vector<std::string> states;
+	std::vector<std::string> inputSymbols;
+	MealyMoves moves;
+
+	std::vector<std::string> elements;
+
+	for (int rowIndex = 0; rowIndex < spreadsheet.size(); ++rowIndex)
+	{
+		for (int columnIndex = 0; columnIndex < spreadsheet[rowIndex].size(); ++columnIndex)
+		{
+			if (rowIndex == 0 && columnIndex == 0)
+			{
+				continue;
+			}
+
+			if (rowIndex == 0)
+			{
+				states.push_back(spreadsheet[rowIndex][columnIndex]);
+				continue;
+			}
+
+			if (columnIndex == 0)
+			{
+				inputSymbols.push_back(spreadsheet[rowIndex][columnIndex]);
+				continue;
+			}
+
+			std::stringstream ss(spreadsheet[rowIndex][columnIndex]);
+			std::string element;
+			while (std::getline(ss, element, '/'))
+			{
+				elements.push_back(element);
+			}
+
+			moves.insert({
+				{spreadsheet[0][columnIndex], spreadsheet[rowIndex][0]},
+				{elements[0], elements[1]}
+			});
+
+			elements.clear();
+		}
+	}
+
 	return {
-		.states = { "a1", "a2", "a3" },
-		.inputSymbols = { "z1", "z2" },
-		.moves = {
-			{ { "a1", "z1" }, { "a3", "w1" } },
-			{ { "a1", "z2" }, { "a1", "w1" } },
-			{ { "a2", "z1" }, { "a1", "w1" } },
-			{ { "a2", "z2" }, { "a3", "w2" } },
-			{ { "a3", "z1" }, { "a1", "w2" } },
-			{ { "a3", "z2" }, { "a2", "w1" } },
-		},
+		.states = states,
+		.inputSymbols = inputSymbols,
+		.moves = moves,
 	};
 }
 
--- lab1/Infrastructure/CSV/CInputOutputAdapter.h
+++ lab1/Infrastructure/CSV/CInputOutputAdapter.h
@@ -5,6 +5,11 @@
 class CInputOutputAdapter : public IInputOutputAdapter
 {
 public:
+	using Spreadsheet = std::vector<std::vector<std::string>>;
+
 	MealyAutomaton GetMealy(const std::string& filename) override;
 	MooreAutomaton GetMoore(const std::string& filename) override;
+
+private:
+	static Spreadsheet GetDataFromFile(const std::string& filename);
 };
